eb8c7b49c2b5c495d8571db0458eeb5c97ce3574
Martin Aceto <martin.aceto@gmail.com>
2023-08-05 12:07:10 -0400

    tests skeleton

---
+++ tests/test_ranks/libs/test_ranks_result.py
@@ -0,0 +1,31 @@
+from functions.ranks.libs.result import Result
+
+def test_success():
+    r = Result(status="success")
+    assert r.success() == True
+    assert r.failure() == False
+
+def test_failure():
+    r = Result(errors=["some error"])
+    assert r.success() == False
+    assert r.failure() == True
+
+def test_default_status():
+    r = Result()
+    assert r.status == "ok"
+    assert r.success() == True
+    assert r.failure() == False
+
+def test_custom_status():
+    r = Result(status="custom")
+    assert r.status == "custom"
+    assert r.success() == True
+    assert r.failure() == False
+
+def test_result():
+    r = Result(result="some result")
+    assert r.result == "some result"
+
+def test_errors():
+    r = Result(errors=["some error"])
+    assert r.errors == ["some error"]
