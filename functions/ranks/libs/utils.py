262dad5c9e563821a6c86c3c43f1fa706e7c4aaa
Martin Aceto <martin.aceto@gmail.com>
2023-08-09 20:30:42 -0400

    change ranks, geos and technologies responses

--- functions/ranks/libs/utils.py
+++ functions/ranks/libs/utils.py
@@ -1,7 +1,5 @@
 import json
 
-RANKS = ["ALL", "Top 100k", "Top 10M", "Top 10k", "Top 1M", "Top 1k"]
-
 def output(result):
   status = 200 if result.success() else 400
   payload = result.result if result.success() else convert_to_hashes(result.errors)
@@ -13,3 +11,5 @@ def convert_to_hashes(arr):
         hash_dict = {inner_arr[0]: inner_arr[1]}
         hashes_arr.append(hash_dict)
     return hashes_arr
+
+RANKS = [ { "num_origins": "9731427", "rank": "ALL" }, { "num_origins": "7232806", "rank": "Top 10M" }, { "num_origins": "881817", "rank": "Top 1M" }, { "num_origins": "91410", "rank": "Top 100k" }, { "num_origins": "9524", "rank": "Top 10k" }, { "num_origins": "965", "rank": "Top 1k" } ]
