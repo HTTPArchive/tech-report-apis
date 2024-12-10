15225bf6888003bd515632011ac8e3eda219cbb8
Martin Aceto <martin.aceto@gmail.com>
2024-12-10 17:14:57 -0500

    new headers in cwvtech

--- functions/cwvtech/main.py
+++ functions/cwvtech/main.py
@@ -1,21 +1,15 @@
 import functions_framework
 
 from .libs.validator import Validator
-from .libs.utils import output
 from .libs.queries import list_data
-from .libs.network import respond_cors
+from .libs.network import respond_cors, respond
 
 @functions_framework.http
 def dispatcher(request):
   
   if request.method == "OPTIONS":
     return respond_cors()
-  
-  headers = {
-     "Access-Control-Allow-Origin": "*",
-     "cache-control": "public, max-age=21600"
-    }
-  
+
   args = request.args.to_dict()
 
   validator = Validator(params=args)
@@ -23,8 +17,8 @@ def dispatcher(request):
 
   if result.failure():
     print("error", result.errors)
-    return output(result)
+    return respond(result)
   
   response = list_data(result.result)
 
-  return output(response, headers)
+  return respond(response)
