aad1d0b0b14583bea47cdfdd5783b097d311816b
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-02-21 23:15:45 +0100

    icon name

--- functions/technologies/libs/presenters.py
+++ functions/technologies/libs/presenters.py
@@ -1,11 +1,10 @@
-
 class Presenters:
   @staticmethod
   def technology(item):
     return {
-      'similar_technologies': item['similar_technologies'],
-      'description': item['description'],
-      'origins': item['origins'],
       'technology': item['technology'],
-      'category': item['category']
+      'category': item['category'],
+      'description': item['description'],
+      'icon': item['icon'],
+      'origins': item['origins']
     }
