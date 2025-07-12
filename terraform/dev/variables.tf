90b6711e20d717abeb85327d0c551990724689ad
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-07-12 14:51:05 +0200

    SA name in source code

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -17,16 +17,16 @@ variable "project_database" {
   description = "The database name"
   default     = "tech-report-api-prod" // TODO: Update this to the DEV database name
 }
-
 variable "google_service_account_cloud_functions" {
   type        = string
   description = "Service account for Cloud Functions"
+  default     = "cloud-function@httparchive.iam.gserviceaccount.com"
 }
 variable "google_service_account_api_gateway" {
   type        = string
   description = "Service account for API Gateway"
+  default     = "api-gateway@httparchive.iam.gserviceaccount.com"
 }
-
 variable "min_instances" {
   description = "(Optional) The limit on the minimum number of function instances that may coexist at a given time."
   type        = number
--- terraform/modules/service-account/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "email" {
-  description = "Email for the service account"
-  value       = google_service_account.service_account.email
-}
-output "member" {
-  description = "The Identity of the service account in the form serviceAccount:{email}. This value is often used to refer to the service account in order to grant IAM permissions."
-  value       = google_service_account.service_account.member
-}
