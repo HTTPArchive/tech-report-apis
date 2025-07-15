db0be01e30d79a5e7c47b2a026ebcca52972e3f9
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-07-15 12:59:02 +0200

    fix paths

--- terraform/modules/run-service/docker.tf
+++ terraform/modules/run-service/docker.tf
@@ -1,15 +1,15 @@
 # Calculate hash of source files to determine if rebuild is needed
 locals {
-  source_files = fileset(path.root, "../${var.function_name}/*")
+  source_files = fileset(path.root, "${var.source_directory}/*")
   source_hash  = substr(sha1(join("", [for f in local.source_files : filesha1(f)])), 0, 8)
 }
 
 # Build Docker image
 resource "docker_image" "function_image" {
-  name = "${var.region}-docker.pkg.dev/${var.project}/dataform/${var.function_name}:${local.source_hash}"
+  name = "${var.region}-docker.pkg.dev/${var.project}/cloud-run/${var.function_name}:${local.source_hash}"
 
   build {
-    context    = "../${var.function_name}/"
+    context    = var.source_directory
     dockerfile = "Dockerfile"
     platform   = "linux/amd64"
   }
