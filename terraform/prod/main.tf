d03f3045316a46de7bc5c75716bb6286a18ff937
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-12-13 00:10:22 +0100

    format

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -21,14 +21,14 @@ provider "google" {
 }
 
 module "endpoints" {
-  source                      = "./../modules/run-service"
-  entry_point                 = "app"
-  project                     = var.project
-  environment                 = var.environment
-  source_directory            = "../../src"
-  function_name               = "tech-report-api"
-  region                      = var.region
-  min_instances               = var.min_instances
+  source           = "./../modules/run-service"
+  entry_point      = "app"
+  project          = var.project
+  environment      = var.environment
+  source_directory = "../../src"
+  function_name    = "tech-report-api"
+  region           = var.region
+  min_instances    = var.min_instances
   environment_variables = {
     "PROJECT"  = var.project
     "DATABASE" = var.project_database
